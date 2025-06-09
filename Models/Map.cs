66748429ca1926fc04b81afcc6f3ffc4e79fd3a7
Nikolaj Roager Christensen <nikolaj@ulvedalen.dk>
2025-06-09 21:23:54 +0200

    Early sketch of loading

---
+++ Models/Map.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using SkiaSharp;
+
+namespace StrategyGameSketch.Models;
+
+public class Map
+{
+    public int Width { get; }
+    public int Height { get; }
+    public Map()
+    {
+        Width = 250;
+        Height = 180;
+    }
+
+    /// <summary>
+    /// C
+    /// </summary>
+    /// <returns></returns>
+    public MemoryStream AsPngImage()
+    {
+        
+        var image = new SKImageInfo(Width,Height);
+        using var bitmap = new SKBitmap(image);
+        using (var canvas = new SKCanvas(bitmap))
+        {
+            canvas.Clear(SKColors. Purple);
+        }
+
+        var imageStream = new MemoryStream();
+        bitmap.Encode(imageStream, SKEncodedImageFormat.Png, 100);
+        imageStream.Position = 0;
+        
+        return imageStream;
+    }
+}
---
+++ Models/Province.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace StrategyGameSketch.Models;
+
+public class Province
+{
+    /// <summary>
+    /// X Location of the province center in the image panel
+    /// </summary>
+    public int X {get; set;}
+    /// <summary>
+    /// Y Location of the province center in the image panel
+    /// </summary>
+    public int Y {get; set;}
+    /// <summary>
+    /// Name and key of this province
+    /// </summary>
+    public string Name { get; set; } = "null";
+    
+    /// <summary>
+    /// The other province names near this thing
+    /// </summary>
+    public List<string> NeighbourNames = new();
+    
+    /// <summary>
+    /// De-jure owner
+    /// </summary>
+    public string Owner { get; set; } = "Ukraine";
+
+    /// <summary>
+    /// De-facto controller
+    /// </summary>
+    public string Controller { get; set; } = "Ukraine";
+    
+    
+}
